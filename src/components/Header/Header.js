50b904a1327cd4a4bd6ead09c03ec1be5c034d89
sheikhrakib0 <rakibrakib0179@gmail.com>
2021-11-18 23:51:32 +0600

    hgda

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -17,11 +17,6 @@ const Header = () => {
             <img src={logo} alt='logo' class="fill-current h-8 w-8 mr-2" width="54" height="54" />
             <span class="font-semibold text-xl tracking-tight">Medicare Plus</span>
           </div>
-          <div class="block lg:hidden">
-            <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-              <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
-            </button>
-          </div>
           <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
             <div class="text-sm lg:flex-grow">
               <Link to="/home" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4 uppercase">
