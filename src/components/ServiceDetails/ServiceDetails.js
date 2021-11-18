571aad26acb692419c0404986899c397b872d2aa
sheikhrakib0 <rakibrakib0179@gmail.com>
2021-11-19 00:11:14 +0600

    lhfg

--- src/components/ServiceDetails/ServiceDetails.js
+++ src/components/ServiceDetails/ServiceDetails.js
@@ -1,6 +1,5 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
-import { useState } from 'react/cjs/react.development';
 
 const ServiceDetails = () => {
   const [service, setService] = useState();
@@ -10,7 +9,7 @@ const ServiceDetails = () => {
     fetch(`http://localhost:3000/servicedetails/${id}`)
     .then(res=>res.json())
     .then(data=>setService(data))
-},[])
+},[id])
   return (
     <div className='container' style={{height: '500px'}}>
       <h2 className="text-4xl pt-4">This is service detials with id:  {id}</h2>
